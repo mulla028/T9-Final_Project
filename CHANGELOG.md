e43eb0dcbc033b4ba15195db5f71acb0fef0f00e
Amir Mullagaliev <138519917+mulla028@users.noreply.github.com>
2025-01-30 16:53:47 -0500

    Merge sp-1 work to main (#36)
    * backend files
    
    * frontend files
    
    * src deleted
    
    * unnecessary files deleted
    
    * client added
    
    * Root files added
    
    * server added
    
    * structure changed
    
    * Registration page implemented (#29)
    
    * Deleted useless lines
    
    * Registration page implemented, and Apple log in deleted as paid option
    
    * changed the name of the package
    
    * Changelog updated to describe new feature
    
    * Forgot Password logic and UI implemented using nodemailer (#28)
    
    * Added forgot password branch
    
    * Described changes
    
    * Reverted
    
    ---------
    
    Co-authored-by: mulla028 <amullagaliev@myseneca.ca>
    Co-authored-by: Amir Mullagaliev <138519917+mulla028@users.noreply.github.com>
    
    * Home page implemented (#27)
    
    * Adjust the home page with the new structure base on branch sp-1 v.1.3
    
    * Update CHANGELOG.md
    
    * described new feature
    
    ---------
    
    Co-authored-by: mulla028 <amullagaliev@myseneca.ca>
    Co-authored-by: Amir Mullagaliev <138519917+mulla028@users.noreply.github.com>
    
    * Updated CHANGELOG.md
    
    ---------
    
    Co-authored-by: Timi Jacobs Uba <timij90@gmail.com>
    Co-authored-by: Gurel Sezgin <82119407+gurelx@users.noreply.github.com>
    Co-authored-by: zgzgzgc <141372836+zgzgzgc@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,14 @@
-# Changelog 
-Every sprint's progress relflected here.
+TESTING PR
+
+# Changelog
+
+Every sprint's progress reflected here.
 
 ## Sprint 1
 
 ### Added
 
-- We have implemented this page bla bla bla. [#11](https://github.com/mulla028/T9-Final_Project/pull/11)
+- Added the homepage [#27](https://github.com/mulla028/T9-Final_Project/pull/27)
+- Registration Page [#29](https://github.com/mulla028/T9-Final_Project/pull/29)
+- Forgot Password logic and UI/UX using `nodemailer` Added [#28](https://github.com/mulla028/T9-Final_Project/pull/28)
+- Login Page logic and UI implemented using OAuth [#23](https://github.com/mulla028/T9-Final_Project/pull/23)
---
+++ server/server.js
@@ -0,0 +1,47 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const passport = require('./config/passport');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const auth = require('./routes/authRoutes');
+const sendPasswordReset = require("./controllers/passwordResetController");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.json());
+app.use(passport.initialize());
+
+// Define Routes
+app.use('/api/auth', auth);
+
+// Send email for forgetting password
+app.post('/api/passwordResetEmail', async (req, res) => {
+    const { email } = req.body;
+    try {
+        await sendPasswordReset.sendPasswordReset(email);
+        res.status(200).send({email});
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+  });
+
+// Send reset password
+app.post('/api/resetPassword', async (req, res) => {
+    const { id } = req.body;
+    const { password } = req.body;
+    try {
+        await sendPasswordReset.resetPassword(id, password);
+        res.status(200).send({id});
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+  });
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
