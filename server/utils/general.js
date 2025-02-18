c6c848c16cb11790aa97a5a8f012159605c48584
mulla028 <amullagaliev@myseneca.ca>
2025-02-17 20:46:45 -0500

    Conflicts resolved

--- server/utils/general.js
+++ server/utils/general.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 export const CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:3000/";
+=======
+export const CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:3000/";
+>>>>>>> 90fccbe (BASE_URL replaced with CALLBACK_URL)
