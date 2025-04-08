ae9daccc6ac704e3f03dce803813245f58cda201
Amir Mullagaliev <138519917+mulla028@users.noreply.github.com>
2025-04-08 17:22:46 -0400

    Revert "Navbar provider template implemented (#112)" (#124)
    This reverts commit d6e5f203a83a0fbfc4b2a18cbd578166b8ef875c.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import { Provider } from "react-redux";
 import { AuthProvider } from "@/context/AuthContext";
 import { LoadScript } from "@react-google-maps/api";
-import Header from "@/components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import store from "@/redux/store";
@@ -14,10 +13,7 @@ export default function App({ Component, pageProps }) {
     <Provider store={store}> 
       <AuthProvider>
         <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY} libraries={libraries}>
-          <Header />
-          <div style={{ paddingTop: "56px" }}>
           <Component {...pageProps} />
-          </div>
         </LoadScript>
       </AuthProvider>
     </Provider>
