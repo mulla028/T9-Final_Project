7729f4d52b9b5584f854c1187fe1940d04d151b9
Amir Mullagaliev <138519917+mulla028@users.noreply.github.com>
2025-04-08 17:22:46 -0400

    Revert "Navbar provider template implemented (#112)" (#124)
    This reverts commit d6e5f203a83a0fbfc4b2a18cbd578166b8ef875c.

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,27 +26,16 @@ const Header = () => {
 
 
     return (
-        <Navbar bg="light" expand="lg" fixed="top">
+        <Navbar bg="light" expand="lg" fixed="top"> {/* Keeps the header fixed */}
             <Container fluid="md">
                 <Navbar.Brand href="/">Driftway</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="m-auto flex">
-                        {isHomePage ? (
-                            // Navigation for homepage
-                            <>
-                                <Nav.Link href="#why-slow-travel" className="no-bold-link">How It Works</Nav.Link>
-                                <Nav.Link href="#hero" className="no-bold-link">Discover</Nav.Link>
-                                <Nav.Link href="#eco-partners" className="no-bold-link">EcoPartnerships</Nav.Link>
-                                <Nav.Link href="#blog-section" className="no-bold-link">Blog</Nav.Link>
-                            </>
-                        ) : (
-                            // Navigation for other pages
-                            <>
-                                <Nav.Link href="/" className="no-bold-link">Home</Nav.Link>
-                                <Nav.Link href="/itinerary" className="no-bold-link">Itinerary Overview</Nav.Link>
-                            </>
-                        )}
+                        <Nav.Link href="#why-slow-travel" className="no-bold-link">How It Works</Nav.Link>
+                        <Nav.Link href="#hero" className="no-bold-link">Discover</Nav.Link>
+                        <Nav.Link href="#eco-partners" className="no-bold-link">EcoPartnerships</Nav.Link>
+                        <Nav.Link href="#blog-section" className="no-bold-link">Blog</Nav.Link>
                     </Nav>
                     <Nav className='d-flex align-items-center gap-4'>
                         {token ? (
