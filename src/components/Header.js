1e0bc7a678263a282e59b9e4101111533ac60b62
mulla028 <amullagaliev@myseneca.ca>
2025-04-08 21:47:38 -0400

    Navbar improved

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,55 +47,60 @@ const Header = () => {
 
 
     return (
-        <Navbar bg="light" expand="lg" fixed="top">
-            <Container fluid="md">
-                 <Navbar.Brand href="/">Driftway</Navbar.Brand>
-                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                 <Navbar.Collapse id="basic-navbar-nav">
-                    {pathname === '/' && (
-                        <Nav className="m-auto flex">
-                            <Nav.Link href="/#why-slow-travel" className="no-bold-link">
-                                How It Works
-                            </Nav.Link>
-                            <Nav.Link href="/#hero" className="no-bold-link">
-                                Discover
-                            </Nav.Link>
-                            <Nav.Link href="/#eco-partners" className="no-bold-link">
-                                EcoPartnerships
-                            </Nav.Link>
-                            <Nav.Link href="/#blog-section" className="no-bold-link">
-                                Blog
-                            </Nav.Link>
-                        </Nav>
-                    )}
-                    <Nav className="d-flex align-items-center gap-4 ms-auto">
-                        {token ? !pathname.startsWith('/user') && (
-                            <>
-                                <div className="icon-button position-relative" onClick={handleClick}>
-                                    {unreadCount > 0 && (
-                                        <span className="notification-badge">
-                                            {unreadCount}
-                                        </span>
-                                    )}
-                                    <FaBell size={24} />
-                                </div>
-                                <span className="me-2 fw-bold">Hi, {firstName}</span>
-                                <ProfileDropdown />
-                            </>
-                        ) : (
-                            <>
-                                <Button variant="success" href="/signup?role=user" className="my-button">
-                                    Sign in
-                                </Button>
-                                <Button variant="success" href="/signup?role=admin" className="my-button">
-                                    Admin
-                                </Button>
-                            </>
-                        )}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <>
+            {!pathname.startsWith('/signup') && (
+
+                <Navbar bg="light" expand="lg" fixed="top">
+                <Container fluid="md">
+                <Navbar.Brand href="/">Driftway</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                {pathname === '/' && (
+                    <Nav className="m-auto flex">
+                    <Nav.Link href="/#why-slow-travel" className="no-bold-link">
+                    How It Works
+                    </Nav.Link>
+                    <Nav.Link href="/#hero" className="no-bold-link">
+                    Discover
+                    </Nav.Link>
+                    <Nav.Link href="/#eco-partners" className="no-bold-link">
+                    EcoPartnerships
+                    </Nav.Link>
+                    <Nav.Link href="/#blog-section" className="no-bold-link">
+                    Blog
+                                    </Nav.Link>
+                                </Nav>
+                            )}
+                            <Nav className="d-flex align-items-center gap-4 ms-auto">
+                                {token ? !pathname.startsWith('/user') && (
+                                    <>
+                                        <div className="icon-button position-relative" onClick={handleClick}>
+                                            {unreadCount > 0 && (
+                                                <span className="notification-badge">
+                                                    {unreadCount}
+                                                </span>
+                                            )}
+                                            <FaBell size={24} />
+                                        </div>
+                                        <span className="me-2 fw-bold">Hi, {firstName}</span>
+                                        <ProfileDropdown />
+                                    </>
+                                ) : (
+                                    <>
+                                        <Button variant="success" href="/signup?role=user" className="my-button">
+                                            Sign in
+                                        </Button>
+                                        <Button variant="success" href="/signup?role=admin" className="my-button">
+                                            Admin
+                                        </Button>
+                                    </>
+                                )}
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            )}
+        </>
     );
 };
 
