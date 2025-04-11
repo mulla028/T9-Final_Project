e91a7e89c2dca09092e2080b5845390ffc2f2b56
zgzgzgc <141372836+zgzgzgc@users.noreply.github.com>
2025-04-10 20:41:43 -0400

    Feedback page (#135)
    * Fix Static Icon for Traveller Feedback issue-95
    
    * Implemented the Feedback page for issue-101
    
    * Improve the stabilty for the Feedback page
    
    * Implemnt dynamic user icon and username for the Feedback page and also add icon in the nav bar
    
    * Implemnt dynamic user icon and username for the Feedback page and also add icon in the nav bar
    
    * Implemnt dynamic user icon and username for the Feedback page and also add icon in the nav bar
    
    * fix something
    
    * fix something
    
    * fix something
    
    * fix something
    
    * fix something
    
    * fix something
    
    * fix the feedback page issue
    
    ---------
    
    Co-authored-by: GuocongZhang <you@gzhang57@myseneca.ca>

--- server/models/Feedback.js
+++ server/models/Feedback.js
@@ -3,13 +3,9 @@ const mongoose = require('mongoose');
 const FeedbackSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    refPath: 'userModel' // 🔥 dynamic reference to User or Social-User
-  },
-  userModel: {
-    type: String,
-    required: true,
-    enum: ['User', 'Social-User'] // must match model names
+    ref: 'User',
+    required: true
+
   },
   title: { type: String },
   comment: { type: String, required: true },
